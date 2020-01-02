fa6a3fbd3473d957308a614dc4fc03dffadcd104
Gordon Taylor <ivyterrace@hotmail.co.uk>
2020-01-02 21:40:31 +0000

    updates to work with ts app

--- app.js
+++ app.js
@@ -10,23 +10,36 @@ let CACHE = {
   images: {}
 };
 
-// Initiate body parser and check size of incoming data
+// // Initiate body parser and check size of incoming data
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use( (req, res, next) => {
-  // Check size of POST body - limit to MAX_DATA_POINTS number of rows
-  if (req.body.coordsArray.length > MAX_DATA_POINTS) {
-    res.status(413).json( "POST request limit exceeded (limited to " + MAX_DATA_POINTS + " points)" );
-  } else {
-      next ();
-  }
+
+// Set up Cross Origin Resource Sharing (CORS )
+app.use( (req, res, next) => { 
+  // inject a header into the response
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Request-With, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");
+  next();
 });
+
+// dont know why this isnt working when called from trailscape...
+//
+// app.use( (req, res, next) => {
+//   // Check size of POST body - limit to MAX_DATA_POINTS number of rows
+//   console.log(req.body);
+//   if (req.body.coordsArray.length > MAX_DATA_POINTS) {
+//     res.status(413).json( "POST request limit exceeded (limited to " + MAX_DATA_POINTS + " points)" );
+//   } else {
+//       next ();
+//   }
+// });
 
 
 /**
  * Get elevations from ASTGTM_v3 data downloaded from https://lpdaac.usgs.gov/products/astgtmv003/
  */ 
-app.post('/elevations/', (req, res) => {
+app.post('/elevations/', (req, res) => { 
 
   // reset CACHE
   CACHE.pixels = {}; // read/write in function readPixels()
@@ -40,7 +53,7 @@ app.post('/elevations/', (req, res) => {
       writeResultsToFile: false
     }
   }
-  
+
   // promise chain running each point sequentially and returning the result as an array
   req.body.coordsArray.reduce( (promise, point) => {
     return promise.then( (allResults) => 
@@ -49,9 +62,9 @@ app.post('/elevations/', (req, res) => {
       ));
   }, Promise.resolve([])).then( result => { 
     res.status(200).json( {result} );
+    console.log(result);
     if (options.writeResultsToFile) { writeResultsToFile(result, options); }
-    }
-  );
+  });
 
 });
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 // type 'nodemon server.js' at the command line
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var port = 3001;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
